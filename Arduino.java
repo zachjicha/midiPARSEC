8a38e034cbbb6a8603569f789dd5464a0bb8a5a4
Zach <zachjicha13@gmail.com>
2019-02-19 18:12:24 -0800

    here's some libraries

---
+++ Arduino.java
@@ -0,0 +1,5 @@
+import SerialPort.*;
+
+public class Arduino {
+  
+}
---
+++ Parsec.java
@@ -0,0 +1,32 @@
+//import arduino.*;
+import java.io.*;
+import java.util.*;
+import gnu.io.*;
+
+public class Parsec {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+      /*
+        Arduino arduino = new Arduino("COM4", 9600);
+        arduino.openConnection();
+        try {
+          Thread.sleep(5000);
+        }
+        catch(InterruptedException e) {
+        }
+        arduino.closeConnection();
+        */
+    }
+
+
+}
