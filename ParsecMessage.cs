12a592d2ce435025237212201d5b75e55213de1c
Zachary Jicha <zjicha@ucsc.edu>
2019-02-21 21:42:28 -0800

    Remove old ParsecMessage constructor

--- ParsecMessage.cs
+++ ParsecMessage.cs
@@ -48,34 +48,6 @@ namespace Parsec
             Console.WriteLine("Track: {5} {0} {1} {2} {3} {4}", message[0], message[1], message[2], message[3], message[4], track);
         }
 
-        public ParsecMessage(byte device, byte code, byte data, uint _time)
-        {
-            time = _time;
-            message = new byte[8];
-            message[0] = 0xAE;
-            message[1] = device;
-            //Byte 2 (Message Length) is handled below
-            message[3] = code;
-            message[4] = data;
-
-            if(device == 0xFF)
-            {
-                message[2] = 1;
-            }
-            else {
-                //Only note on events carry any extra data
-                if(code == 0xA2)
-                {
-                    message[2] = 2;
-                }
-                else
-                {
-                    message[2] = 1;
-                }
-            }
-        }
-
-        //Silent data constructor
         public ParsecMessage(byte device, byte code, byte data, uint _time, uint _silentData)
         {
             time = _time;
